97b8b47c257884682bdc41749122b6ef00cb0c23
thisisommore <ommore501@gmail.com>
2022-08-09 16:26:57 +0000

    custodial: init: set cosmos config

--- custodial/app/stage/appinit/init.go
+++ custodial/app/stage/appinit/init.go
@@ -2,12 +2,14 @@
 package appinit
 
 import (
+	"github.com/MyriadFlow/cosmos-wallet/custodial/app/stage/appinit/cosmosinit"
 	"github.com/MyriadFlow/cosmos-wallet/custodial/app/stage/appinit/dbconinit"
 	"github.com/MyriadFlow/cosmos-wallet/custodial/app/stage/appinit/dbmigrate"
 	"github.com/MyriadFlow/cosmos-wallet/custodial/app/stage/appinit/logoinit"
 )
 
 func Init() {
+	cosmosinit.Init()
 	logoinit.Init()
 	dbconinit.Init()
 	dbmigrate.Migrate()
